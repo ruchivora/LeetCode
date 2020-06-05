b2ca48d2636ba4ce2e9a626e5816f2a1d935a0ed
ruchivora <ruchivora16@gmail.com>
2020-06-05 16:55:46 +0530

    Important Question

---
+++ BinarySearch/6.SearchInsertPosition.java
@@ -0,0 +1,33 @@
+/*
+	Important note : In Binary Search even if you 
+	don't find the given target , your mid element
+	after the while loop will land somewhere nearby .
+*/
+class Solution {
+    {
+    	if( target < nums[0] ) return 0 ;
+    	if( target > nums[nums.length-1] ) return nums.length ;
+
+    	int left  = 0 ;
+    	int right = nums.length - 1 ;
+    	int mid = 0 ;
+
+    	while( left <= right )
+    	{
+    		mid = left + ( right - left ) / 2 ;
+
+    		if( nums[mid] == target )
+    			return mid ;
+
+    		else if( nums[mid] > target )
+    			     right = mid - 1 ;
+    		
+    			  else left = mid + 1 ;		
+    	}
+
+    	if( nums[mid] < target )
+    		return mid + 1 ; 
+
+    return mid ;
+    }
+}
